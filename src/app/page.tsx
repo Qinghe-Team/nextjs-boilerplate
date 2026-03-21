acad5768c79f92e5e26efc8bcf1d06cf6172473f
9xhk-1 <mail@9x.hk>
2026-03-21 10:56:37 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import Button from "./_components/Button";
 function Main()
 {
   return (
@@ -67,6 +67,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Main/>
+      <Button/>
       <Main/>
     </div>
   );
